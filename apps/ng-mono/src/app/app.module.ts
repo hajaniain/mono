c737f52172b642d87343047eb61e67ecc4a838a4
hajaniain <hajaniainamarius@gmail.com>
2020-02-24 14:40:09 +0100

    create hello component in core lib and use it

--- apps/ng-mono/src/app/app.module.ts
+++ apps/ng-mono/src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { HelloModule } from '@mono/core';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, HelloModule],
   providers: [],
   bootstrap: [AppComponent]
 })
