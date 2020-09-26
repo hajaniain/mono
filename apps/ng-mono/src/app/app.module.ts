8e8cd7c6b486d2f1a2e8f42c47ea9d34a8fc62e6
hajaniain <hajaniainamarius@gmail.com>
2020-09-26 21:53:49 +0200

    feat: use angular material

--- apps/ng-mono/src/app/app.module.ts
+++ apps/ng-mono/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { CoreModule } from '@mono/core';
 import { AppComponent } from './app.component';
 import { MyCounterComponent } from './my-counter/my-counter.component';
 import { StateModule } from '@mono/state';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent, MyCounterComponent],
-  imports: [BrowserModule, CoreModule, StateModule],
+  imports: [
+    BrowserModule,
+    CoreModule,
+    StateModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
